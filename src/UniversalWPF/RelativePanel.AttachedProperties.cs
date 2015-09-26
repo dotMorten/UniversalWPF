057889ab13ad056ee28cfde3eef7131a98830055
mort5161 <mnielsen@esri.com>
2015-09-26 12:09:09 -0700

    Initial commit - work in progress

---
+++ src/UniversalWPF/RelativePanel.AttachedProperties.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UniversalWPF
+{
+	public partial class RelativePanel
+	{
+		private static void OnAlignPropertiesChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			var elm = d as FrameworkElement;
+			if (elm.Parent is FrameworkElement)
+				((FrameworkElement)elm.Parent).InvalidateArrange();
+		}
+
+		public static object GetAbove(DependencyObject obj)
+		{
+			return (object)obj.GetValue(AboveProperty);
+		}
+
+		public static void SetAbove(DependencyObject obj, object value)
+		{
+			obj.SetValue(AboveProperty, value);
+		}
+
+		public static readonly DependencyProperty AboveProperty =
+			DependencyProperty.RegisterAttached("Above", typeof(object), typeof(RelativePanel), new PropertyMetadata(null, OnAlignPropertiesChanged));
+		
+
+		public static bool GetAlignBottomWithPanel(DependencyObject obj)
+		{
+			return (bool)obj.GetValue(AlignBottomWithPanelProperty);
+		}
+
+		public static void SetAlignBottomWithPanel(DependencyObject obj, bool value)
+		{
+			obj.SetValue(AlignBottomWithPanelProperty, value);
+		}
+
+		public static readonly DependencyProperty AlignBottomWithPanelProperty =
+			DependencyProperty.RegisterAttached("AlignBottomWithPanel", typeof(bool), typeof(RelativePanel), new PropertyMetadata(false, OnAlignPropertiesChanged));
+
+		public static object GetAlignBottomWith(DependencyObject obj)
+		{
+			return (object)obj.GetValue(AlignBottomWithProperty);
+		}
+
+		public static void SetAlignBottomWith(DependencyObject obj, object value)
+		{
+			obj.SetValue(AlignBottomWithProperty, value);
+		}
+
+		public static readonly DependencyProperty AlignBottomWithProperty =
+			DependencyProperty.RegisterAttached("AlignBottomWith", typeof(object), typeof(RelativePanel), new PropertyMetadata(null, OnAlignPropertiesChanged));
+
+		public static bool GetAlignHorizontalCenterWithPanel(DependencyObject obj)
+		{
+			return (bool)obj.GetValue(AlignHorizontalCenterWithPanelProperty);
+		}
+
+		public static void SetAlignHorizontalCenterWithPanel(DependencyObject obj, bool value)
+		{
+			obj.SetValue(AlignHorizontalCenterWithPanelProperty, value);
+		}
+
+		public static readonly DependencyProperty AlignHorizontalCenterWithPanelProperty =
+			DependencyProperty.RegisterAttached("AlignHorizontalCenterWithPanel", typeof(bool), typeof(RelativePanel), new PropertyMetadata(false, OnAlignPropertiesChanged));
+
+		public static object GetAlignHorizontalCenterWith(DependencyObject obj)
+		{
+			return (object)obj.GetValue(AlignHorizontalCenterWithProperty);
+		}
+
+		public static void SetAlignHorizontalCenterWith(DependencyObject obj, object value)
+		{
+			obj.SetValue(AlignHorizontalCenterWithProperty, value);
+		}
+
+		public static readonly DependencyProperty AlignHorizontalCenterWithProperty =
+			DependencyProperty.RegisterAttached("AlignHorizontalCenterWith", typeof(object), typeof(RelativePanel), new PropertyMetadata(null, OnAlignPropertiesChanged));
+
+
+
+		public static bool GetAlignLeftWithPanel(DependencyObject obj)
+		{
+			return (bool)obj.GetValue(AlignLeftWithPanelProperty);
+		}
+
+		public static void SetAlignLeftWithPanel(DependencyObject obj, bool value)
+		{
+			obj.SetValue(AlignLeftWithPanelProperty, value);
+		}
+
+
+		public static readonly DependencyProperty AlignLeftWithPanelProperty =
+			DependencyProperty.RegisterAttached("AlignLeftWithPanel", typeof(bool), typeof(RelativePanel), new PropertyMetadata(false, OnAlignPropertiesChanged));
+
+
+		public static object GetAlignLeftWith(DependencyObject obj)
+		{
+			return (object)obj.GetValue(AlignLeftWithProperty);
+		}
+
+		public static void SetAlignLeftWith(DependencyObject obj, object value)
+		{
+			obj.SetValue(AlignLeftWithProperty, value);
+		}
+
+		// Using a DependencyProperty as the backing store for AlignLeftWith.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty AlignLeftWithProperty =
+			DependencyProperty.RegisterAttached("AlignLeftWith", typeof(object), typeof(RelativePanel), new PropertyMetadata(null, OnAlignPropertiesChanged));
+
+
+		public static bool GetAlignRightWithPanel(DependencyObject obj)
+		{
+			return (bool)obj.GetValue(AlignRightWithPanelProperty);
+		}
+
+		public static void SetAlignRightWithPanel(DependencyObject obj, bool value)
+		{
+			obj.SetValue(AlignRightWithPanelProperty, value);
+		}
+
+		public static readonly DependencyProperty AlignRightWithPanelProperty =
+			DependencyProperty.RegisterAttached("AlignRightWithPanel", typeof(bool), typeof(RelativePanel), new PropertyMetadata(false, OnAlignPropertiesChanged));
+
+		public static object GetAlignRightWith(DependencyObject obj)
+		{
+			return (object)obj.GetValue(AlignRightWithProperty);
+		}
+
+		public static void SetAlignRightWith(DependencyObject obj, object value)
+		{
+			obj.SetValue(AlignRightWithProperty, value);
+		}
+
+		public static readonly DependencyProperty AlignRightWithProperty =
+			DependencyProperty.RegisterAttached("AlignRightWith", typeof(object), typeof(RelativePanel), new PropertyMetadata(null, OnAlignPropertiesChanged));
+
+		public static bool GetAlignTopWithPanel(DependencyObject obj)
+		{
+			return (bool)obj.GetValue(AlignTopWithPanelProperty);
+		}
+
+		public static void SetAlignTopWithPanel(DependencyObject obj, bool value)
+		{
+			obj.SetValue(AlignTopWithPanelProperty, value);
+		}
+
+		public static readonly DependencyProperty AlignTopWithPanelProperty =
+			DependencyProperty.RegisterAttached("AlignTopWithPanel", typeof(bool), typeof(RelativePanel), new PropertyMetadata(false, OnAlignPropertiesChanged));
+
+
+
+		public static object GetAlignTopWith(DependencyObject obj)
+		{
+			return (object)obj.GetValue(AlignTopWithProperty);
+		}
+
+		public static void SetAlignTopWith(DependencyObject obj, object value)
+		{
+			obj.SetValue(AlignTopWithProperty, value);
+		}
+
+		public static readonly DependencyProperty AlignTopWithProperty =
+			DependencyProperty.RegisterAttached("AlignTopWith", typeof(object), typeof(RelativePanel), new PropertyMetadata(null, OnAlignPropertiesChanged));
+
+
+
+		public static bool GetAlignVerticalCenterWithPanel(DependencyObject obj)
+		{
+			return (bool)obj.GetValue(AlignVerticalCenterWithPanelProperty);
+		}
+
+		public static void SetAlignVerticalCenterWithPanel(DependencyObject obj, bool value)
+		{
+			obj.SetValue(AlignVerticalCenterWithPanelProperty, value);
+		}
+
+		// Using a DependencyProperty as the backing store for AlignVerticalCenterWithPanel.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty AlignVerticalCenterWithPanelProperty =
+			DependencyProperty.RegisterAttached("AlignVerticalCenterWithPanel", typeof(bool), typeof(RelativePanel), new PropertyMetadata(false, OnAlignPropertiesChanged));
+
+
+
+
+		public static object GetAlignVerticalCenterWith(DependencyObject obj)
+		{
+			return (object)obj.GetValue(AlignVerticalCenterWithProperty);
+		}
+
+		public static void SetAlignVerticalCenterWith(DependencyObject obj, object value)
+		{
+			obj.SetValue(AlignVerticalCenterWithProperty, value);
+		}
+
+		public static readonly DependencyProperty AlignVerticalCenterWithProperty =
+			DependencyProperty.RegisterAttached("AlignVerticalCenterWith", typeof(object), typeof(RelativePanel), new PropertyMetadata(null, OnAlignPropertiesChanged));
+
+		public static object GetBelow(DependencyObject obj)
+		{
+			return (object)obj.GetValue(BelowProperty);
+		}
+
+		public static void SetBelow(DependencyObject obj, object value)
+		{
+			obj.SetValue(BelowProperty, value);
+		}
+
+		public static readonly DependencyProperty BelowProperty =
+			DependencyProperty.RegisterAttached("Below", typeof(object), typeof(RelativePanel), new PropertyMetadata(null, OnAlignPropertiesChanged));
+
+		public static object GetLeftOf(DependencyObject obj)
+		{
+			return (object)obj.GetValue(LeftOfProperty);
+		}
+
+		public static void SetLeftOf(DependencyObject obj, object value)
+		{
+			obj.SetValue(LeftOfProperty, value);
+		}
+
+		public static readonly DependencyProperty LeftOfProperty =
+			DependencyProperty.RegisterAttached("LeftOf", typeof(object), typeof(RelativePanel), new PropertyMetadata(null, OnAlignPropertiesChanged));
+
+		public static object GetRightOf(DependencyObject obj)
+		{
+			return (object)obj.GetValue(RightOfProperty);
+		}
+
+		public static void SetRightOf(DependencyObject obj, object value)
+		{
+			obj.SetValue(RightOfProperty, value);
+		}
+
+		// Using a DependencyProperty as the backing store for RightOf.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty RightOfProperty =
+			DependencyProperty.RegisterAttached("RightOf", typeof(object), typeof(RelativePanel), new PropertyMetadata(null, OnAlignPropertiesChanged));
+
+
+
+	}
+}
---
+++ src/UniversalWPF/RelativePanel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UniversalWPF
+{
+    public partial class RelativePanel : Panel
+    {
+		//static RelativePanel()
+		//{
+		//    DefaultStyleKeyProperty.OverrideMetadata(typeof(RelativePanel), new FrameworkPropertyMetadata(typeof(RelativePanel)));
+		//}
+
+		protected override Size MeasureOverride(Size availableSize)
+		{
+			foreach(var child in Children.OfType<FrameworkElement>())
+			{
+				child.Measure(availableSize);
+			}
+			return base.MeasureOverride(availableSize);
+		}
+
+		/*
+			Default position
+
+			By default, any unconstrained element declared as a child of the RelativePanel is given the entire
+			available space and positioned at the (0, 0) coordinates (upper left corner) of the panel. So, if you
+			are positioning a second element relative to an unconstrained element, keep in mind that the second 
+			element might get pushed out of the panel. 
+
+			Conflicting relationships
+
+			If you set multiple relationships that target the same edge of an element, you might have conflicting
+			relationships in your layout as a result. When this happens, the relationships are applied in the 
+			following order of priority:
+			  •   Panel alignment relationships (AlignTopWithPanel, AlignLeftWithPanel, …) are applied first.
+			  •   Sibling alignment relationships (AlignTopWith, AlignLeftWith, …) are applied second.
+			  •   Sibling positional relationships (Above, Below, RightOf, LeftOf) are applied last.
+
+			The panel-center alignment properties (AlignVerticalCenterWith, AlignHorizontalCenterWithPanel, ...) are
+			typically used independently of other constraints and are applied if there is no conflict.
+
+			The HorizontalAlignment and VerticalAlignment properties on UI elements are applied after relationship 
+			properties are evaluated and applied. These properties control the placement of the element within the
+			available size for the element, if the desired size is smaller than the available size.
+
+		*/
+
+		protected override Size ArrangeOverride(Size finalSize)
+		{
+			Dictionary<string, object> elements = new Dictionary<string, object>();
+			foreach (var child in Children.OfType<FrameworkElement>().Where(c => c.Name != null))
+			{
+				elements[child.Name] = child;
+			}
+			foreach(var child in Children.OfType<FrameworkElement>())
+			{
+				double left = 0;
+				double top = 0;
+				double width = child.DesiredSize.Width;
+				double height = child.DesiredSize.Height;
+				var rightWidth = GetAlignRightWith(child);
+				if(rightWidth is string)
+				{
+					if (!elements.ContainsKey((string)rightWidth))
+						throw new ArgumentException(string.Format("RelativePanel error: The name '{0}' does not exist in the current context", rightWidth));
+					rightWidth = elements[(string)rightWidth];
+				}
+				if (rightWidth is FrameworkElement)
+					left = ((FrameworkElement)rightWidth).DesiredSize.Width;
+				else if (GetAlignLeftWithPanel(child))
+					left = 0;
+				if (GetAlignRightWithPanel(child))
+					width = finalSize.Width - left;
+				if (GetAlignTopWithPanel(child))
+					top = 0;
+				if (GetAlignBottomWithPanel(child))
+					height = finalSize.Height - top;
+				//if (GetAlignRightWithPanel(child))
+				//	height = finalSize.Width - left;
+				child.Arrange(new Rect(left, top, width, height));
+			}
+			return base.ArrangeOverride(finalSize);
+		}
+	}
+}
