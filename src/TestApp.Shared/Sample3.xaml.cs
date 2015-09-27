a4c77a6ed11eb903ed5e0a76a2bdff5d4336a181
Morten Nielsen <mn@iter.dk>
2015-09-27 10:25:45 -0700

    More tests....

---
+++ src/TestApp.Shared/Sample3.xaml.cs
@@ -0,0 +1,16 @@
+﻿#if NETFX_CORE
+using Windows.UI.Xaml.Controls;
+#else
+using System.Windows.Controls;
+#endif
+
+namespace TestApp.Shared
+{
+    public sealed partial class Sample3 : UserControl
+    {
+        public Sample3()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
