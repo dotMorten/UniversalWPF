54f92964f159c1a7911ae24a5f012dd82858129d
Morten Nielsen <mn@iter.dk>
2015-09-27 10:17:37 -0700

    Added some shared xaml to easily compare layout with UWP (hey! This also proves the XAML compatibility! :-)

---
+++ src/TestApp.Shared/Sample1.xaml.cs
@@ -0,0 +1,16 @@
+﻿#if NETFX_CORE
+using Windows.UI.Xaml.Controls;
+#else
+using System.Windows.Controls;
+#endif
+
+namespace TestApp.Shared
+{
+    public sealed partial class Sample1 : UserControl
+    {
+        public Sample1()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ src/TestApp.Shared/Sample2.xaml.cs
@@ -0,0 +1,16 @@
+﻿#if NETFX_CORE
+using Windows.UI.Xaml.Controls;
+#else
+using System.Windows.Controls;
+#endif
+
+namespace TestApp.Shared
+{
+    public sealed partial class Sample2 : UserControl
+    {
+        public Sample2()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
