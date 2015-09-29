46917dc87e5e598d294fd1caebe99b53edd8d412
mort5161 <mnielsen@esri.com>
2015-09-28 20:54:02 -0700

    Added SplitView sample

---
+++ src/TestApp.Shared/SplitView/SV_Sample1.xaml.cs
@@ -0,0 +1,16 @@
+﻿#if NETFX_CORE
+using Windows.UI.Xaml.Controls;
+#else
+using System.Windows.Controls;
+#endif
+
+namespace TestApp.Shared.SplitViews
+{
+    public sealed partial class Sample1 : UserControl
+    {
+        public Sample1()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
