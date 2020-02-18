c505c06d1d79d91d3f64b5b327c5d08337f0948b
Morten <mnielsen@esri.com>
2020-02-18 11:39:10 -0800

    Licensing and missing doc

--- src/UniversalWPF.TwoPaneView/TwoPaneViewPriority.cs
+++ src/UniversalWPF.TwoPaneView/TwoPaneViewPriority.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿/*
+Based on a port of TwoPaneView control in the WinUI Library: https://github.com/microsoft/microsoft-ui-xaml/
+    MIT License
+
+    Copyright (c) Microsoft Corporation. All rights reserved.
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE
+*/
 
 namespace UniversalWPF
 {
+    /// <summary>
+    /// Defines constants that specify which pane has priority in a TwoPaneView.
+    /// </summary>
+    /// <seealso cref="TwoPaneView"/>
+    /// <seealso cref="TwoPaneView.PanePriority"/>
     public enum TwoPaneViewPriority
     {
+        /// <summary>
+        /// Pane 1 has priority.
+        /// </summary>
         Pane1 = 0,
+        /// <summary>
+        /// Pane 2 has priority.
+        /// </summary>
         Pane2 = 1,
     }
 }
